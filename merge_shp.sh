93bff8f19253edf51add86211008a6c99fef1158
Anders Johan Konnestad <ankonnes@nmbu.no>
2019-04-12 10:42:42 +0200

    Update merge_shp.sh

--- merge_shp.sh
+++ merge_shp.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 
 ## merge all shapefiles in a directory
+## and export it to a subdirectory
 
+# make folder for result
+mkdir merge >/dev/null
 
-mkdir final >/dev/null
-
-file="final/merge.shp"
+file="merge/merge.shp"
 nr=0
 
 for i in $(ls *.shp) ; do
-      # utpakking fra mappe $(ls) #over
-      #if [[ "$i" != *.sh ]]; then
-      #  mv $i/* .
-      #fi
-      
-      #echo $i/$i.shp
-      
       if [ -f "$file" ] ; then
         if [ ${nr} -eq 0  ] ; then
           #copy the first file!
-          ogr2ogr -f "ESRI Shapefile" -t_srs EPSG:4326 $file $i
+          ogr2ogr -f "ESRI Shapefile" -t_srs EPSG:4326 $final $i
         else
           #Merge with the next shapefile
-             ogr2ogr -f "ESRI Shapefile" -t_srs EPSG:4326 -update -append $file $i -nln merge
+          ogr2ogr -f "ESRI Shapefile" -t_srs EPSG:4326 -update -append $final $i -nln merge
         fi
       fi
 
